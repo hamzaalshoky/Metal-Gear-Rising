9394585d862de3615a16f593f2129fece4481dd9
nikgub_ <danger8217.nik@gmail.com>
2023-07-23 03:19:39 +0300

    Updated ModItems

--- src/main/java/net/itshamza/mgrr/item/ModItems.java
+++ src/main/java/net/itshamza/mgrr/item/ModItems.java
@@ -12,18 +12,12 @@ import net.minecraftforge.registries.RegistryObject;
 public class ModItems {
     public static final DeferredRegister<Item> ITEMS =
             DeferredRegister.create(ForgeRegistries.ITEMS, MGRRMod.MOD_ID);
-
+    // weapons
+    public static final RegistryObject<Item> HIGH_FREQUENCY_BLADE = ITEMS.register("high_frequency_blade", HighFrequencyBladeItem::new);
+    // spawn eggs
     public static final RegistryObject<Item> CYBORG_SPAWN_EGG = ITEMS.register("cyborg_spawn_egg",
                 () -> new ForgeSpawnEggItem(ModEntityCreator.CYBORG,14265190, 1973794,
                         new Item.Properties()));
-
-   //public static final RegistryObject<Item> POISONOUS_SPINE = ITEMS.register("poisonous_spine",
-    //       () -> new Item(new Item.Properties().tab(ModCreativeModeTabs.AFRICA_TAB)));
-
-  // public static final RegistryObject<Item> POISONOUS_SPEAR = ITEMS.register("poisonous_spear",
-  //         () -> new SwordItem(Tiers.IRON, 3, -1.0f,
-  //                  new Item.Properties().tab(ModCreativeModeTabs.AFRICA_TAB)));
-
    public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
     }
